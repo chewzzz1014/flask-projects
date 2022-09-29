0c5161f56624cbfbc6a5fca4746c418bf7dad597
chewzzz <chewziqing@gmail.com>
2022-09-29 15:42:55 +0800

    Add flask app configuration

--- weather_app/app.py
+++ weather_app/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+# add configuration
+# app.config.from_object("settings")
+app.config.from_envvar("FLASK_CONF_VAR")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 app.run()
