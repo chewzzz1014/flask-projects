61891e02cb36092d358741b0603200464fde364e
chewzzz <chewziqing@gmail.com>
2022-10-12 20:01:05 +0800

    feat: add route about

--- flask-blog/flaskblog.py
+++ flask-blog/flaskblog.py
@@ -4,12 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+@app.route("/home")
+def home():
     return '<h1>Hello World!</h1>'
 
-
+@app.route("/about")
+def about():
+    return '<h1>About!</h1>'
 
 
 if __name__ == "__main__":
-    # run in debug mode
+    # run in debug mode (we can see changes after refresh w/o restart flask server)
     app.run(debug=True)
