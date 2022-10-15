8b2b5c790b3998312f22d810a8e3ed9f7e5df8c8
chewzzz <chewziqing@gmail.com>
2022-10-15 22:43:31 +0800

    Add register form and validation

--- flask-blog/flaskblog.py
+++ flask-blog/flaskblog.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 # __name__ is directory name
 app = Flask(__name__)
+
+# set secret key
+app.config['SECRET_KEY'] = '3a9d3d30d839272b9401294f3cf59ebf'
 
 posts = [
     {
@@ -29,6 +33,20 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # flash message (one time alert)
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 if __name__ == "__main__":
     # run in debug mode (we can see changes after refresh w/o restart flask server)
