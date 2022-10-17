0f927861e03f621248562c1afb28ce317f26fd46
chewzzz <chewziqing@gmail.com>
2022-10-17 15:21:43 +0800

    Add sqlalchemy

--- flask-blog/flaskblog.py
+++ flask-blog/flaskblog.py
@@ -1,11 +1,47 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # __name__ is directory name
 app = Flask(__name__)
 
 # set secret key
 app.config['SECRET_KEY'] = '3a9d3d30d839272b9401294f3cf59ebf'
+# configure database link
+#  link is relative path to file
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+# instance of database
+db = SQLAlchemy(app)
+
+# orm class
+class User(db.Model):
+    # primary key
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email= db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    # connect to Post class through author
+    # lazy=True so that sqlalchemy will load data from database when necessary in one go
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# orm class
+class Post(db.Model):
+    # primary key
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    contents = db.Column(db.Text, nullable=False)
+    # user_id is foreign key
+    user_id = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
